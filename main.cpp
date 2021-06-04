92291bf8be6a9817072fc8842cf937ff03ced696
Joshua <jravaell@gmail.com>
2021-06-04 08:42:34 +0000

    change #2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
 using namespace std;
 
+class login{
+private:
+    string name = "Joshua", pw = "1234";
+    string inpName, inpPw;
+
+public:
+    void login(){
+        cout << "Name: ";
+        cin >> inpName;
+
+        cout << "Password: ";
+        cin >> inpPw;
+
+        if(inpName == name && inpPw == pw){
+            cout << "login Sucess";
+        }
+    }
+};
+
+
+
 int main(){
-    cout << "Test";
+    main m;
+    m.login();
 
     return 0;
 }
